700e53d2fa2447d6e6b8f8d6ccd13a42b05da2b2
SharmaRavindra <sharma.ravi1210@gmail.com>
2013-09-25 02:34:12 +0530

    My First Commit

---
+++ Linkedn.java
@@ -0,0 +1,29 @@
+package ORPackage;
+
+public class Linkedn {
+
+	//Register-
+		public String firstName = "firstName-coldRegistrationForm"; //id
+		public String lastName = "lastName-coldRegistrationForm";   //id
+		public String email = "email-coldRegistrationForm";         //id
+		public String password = "password-coldRegistrationForm";   //id
+		public String submit = "btn-submit";						 //id
+
+		
+		
+		// Sign-In- 
+		public String SignIn = ".//*[@id='top-header']/div/ul/li[3]/a";						//id
+		public String userName = "#session_key-login";				//CSS
+		public String Pwd = "#session_password-login";			    //CSS
+		//String UserName = "session_key-login";				//id
+		//String Pwd = "#session_password-login";			    //id
+		public String SignInBtn = "#btn-primary";				//CSS	
+		
+		// Home page
+		public String accountName = "account-toggle"; // Class name
+		public String SignOut = "Sign Out"; 			//  Link Text
+		public String profile = "Profile";				// LinkText
+		public String numberOfConnections = "member-connections"; 	//Class name
+	
+	
+}
